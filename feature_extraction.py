34f3862d5217a4d78e3cbcde3348d17c0146f4b8
Vern Francisco <avernethy@gmail.com>
2018-05-22 20:18:01 -0500

    copy paste solution to see if we can run

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,6 +1,14 @@
 import pickle
 import tensorflow as tf
+import numpy as np
 # TODO: import Keras layers you need here
+#from keras.models import Sequential
+from keras.layers.core import Dense, Activation, Flatten, Dropout
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+from keras.layers import Input
+from keras.models import Model
+
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
@@ -8,6 +16,8 @@ FLAGS = flags.FLAGS
 # command line flags
 flags.DEFINE_string('training_file', '', "Bottleneck features training file (.p)")
 flags.DEFINE_string('validation_file', '', "Bottleneck features validation file (.p)")
+flags.DEFINE_integer('epochs', 50, "The number of epochs.")
+flags.DEFINE_integer('batch_size', 256, "The batch size.")
 
 
 def load_bottleneck_data(training_file, validation_file):
@@ -37,7 +47,7 @@ def load_bottleneck_data(training_file, validation_file):
 def main(_):
     # load bottleneck data
     X_train, y_train, X_val, y_val = load_bottleneck_data(FLAGS.training_file, FLAGS.validation_file)
-
+    n_classes = len(np.unique(y_train))
     print(X_train.shape, y_train.shape)
     print(X_val.shape, y_val.shape)
 
@@ -46,8 +56,15 @@ def main(_):
     # the dataset
     # 10 for cifar10
     # 43 for traffic
-
+    input_shape = X_train.shape[1:]
+    inp = Input(shape=input_shape)
+    x = Flatten()(inp)
+    x = Dense(n_classes, activation='softmax')(x)
     # TODO: train your model here
+    #X_normalized = np.array((X_train -128) /128 )
+    model = Model(inp, x)
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    model.fit(X_train, y_train, batch_size=FLAGS.batch_size, nb_epoch=FLAGS.epochs,  validation_data=(X_val, y_val), shuffle=True)
 
 
 # parses flags and calls the `main` function above
